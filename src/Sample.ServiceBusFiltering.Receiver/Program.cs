0aeb7ce16d976dfae580cece420768a16bf63c0a
Matt Ridgway <matt.ridgway@zupa.co.uk>
2018-12-23 22:13:16 +0000

    Added empty console projects to send/recv messages on service bus

---
+++ src/Sample.ServiceBusFiltering.Receiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample.ServiceBusFiltering.Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
