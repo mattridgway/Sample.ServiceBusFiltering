6f2c614750417c3f39a86eb681c509d428311b44
Matt Ridgway <matt.ridgway@zupa.co.uk>
2018-12-24 00:18:43 +0000

    Updated the receiver to listen for messages

--- src/Sample.ServiceBusFiltering.Receiver/Program.cs
+++ src/Sample.ServiceBusFiltering.Receiver/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
 
 namespace Sample.ServiceBusFiltering.Receiver
 {
@@ -6,7 +9,45 @@ namespace Sample.ServiceBusFiltering.Receiver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets("32874672-80BB-4246-85E2-0D29BE2F19A5")
+                .AddJsonFile("appsettings.json", false, false)
+                .Build();
+
+            using (var receiver = new Receiver(config["serviceBus:connectionString"], config["serviceBus:topicName"], config["serviceBus:subscriptionName"]))
+            {
+                Console.WriteLine("Receiving Messages, press return to stop");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    internal class Receiver : IDisposable
+    {
+        private readonly SubscriptionClient _subscriptionClient;
+
+        public Receiver(string connectionString, string topicName, string subscriptionName)
+        {
+            _subscriptionClient = new SubscriptionClient(
+                connectionString,
+                topicName,
+                subscriptionName);
+
+            _subscriptionClient.RegisterMessageHandler(
+                (message, cancellation) => {
+                    var alertBody = System.Text.Encoding.UTF8.GetString(message.Body);
+                    Console.WriteLine(alertBody);
+
+                    return Task.CompletedTask;
+                }, 
+                (exceptionArgs) => {
+                    return Task.CompletedTask;
+                });
+        }
+
+        public void Dispose()
+        {
+            _subscriptionClient.CloseAsync().GetAwaiter().GetResult();
         }
     }
 }
